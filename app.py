afb4413e2260fdff3c8ce60717c6ebdd4312ef25
SidharthAgarwal18 <sidharthagarwal2001@gmail.com>
2022-02-22 19:19:49 +0530

    html left

---
+++ app.py
@@ -0,0 +1,26 @@
+import psycopg2
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+country = 'in'          #default country is india..
+suffix  = 'videos'
+gen_attr = 'video_id,title,channel_title,publish_time,tags,views,likes,dislikes'
+
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database='group_23',
+                            user='postgres',
+                            password='12345')
+    return conn
+
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT '+ gen_attr + ' FROM ' + country+suffix+' LIMIT 30;')
+    videos = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', videos=videos)
