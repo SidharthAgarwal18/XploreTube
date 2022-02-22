fe041f7755cd42e3fa7ae9cb67d030286bf2033a
SidharthAgarwal18 <sidharthagarwal2001@gmail.com>
2022-02-23 00:57:34 +0530

    basic working

--- FRONT_END/app.py
+++ FRONT_END/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 country = 'in'          #default country is india..
 suffix  = 'videos'
-gen_attr = 'video_id,title,channel_title,publish_time,tags,views,likes,dislikes'
+gen_attr = 'video_id,title,channel_title,publish_time,description,views,likes,dislikes'
 
 def get_db_connection():
     conn = psycopg2.connect(host='localhost',
@@ -19,8 +19,14 @@ def get_db_connection():
 def index():
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('SELECT '+ gen_attr + ' FROM ' + country+suffix+' LIMIT 30;')
+    cur.execute('SELECT '+ gen_attr + ' FROM ' + country+suffix+' ORDER BY views DESC,likes DESC, dislikes LIMIT 30;')
     videos = cur.fetchall()
     cur.close()
     conn.close()
     return render_template('index.html', videos=videos)
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
